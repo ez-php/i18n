01d303d74bde5d880e632942601f9376723a7f1e
home <andreas.uretschnig@gmail.com>
2026-03-15 23:16:34 +0100

    0.3.0

--- src/TranslatorServiceProvider.php
+++ src/TranslatorServiceProvider.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace EzPhp\I18n;
 
-use EzPhp\Application\Application;
-use EzPhp\Config\Config;
-use EzPhp\ServiceProvider\ServiceProvider;
+use EzPhp\Contracts\ConfigInterface;
+use EzPhp\Contracts\ContainerInterface;
+use EzPhp\Contracts\ServiceProvider;
 
 /**
  * Class TranslatorServiceProvider
@@ -20,13 +20,14 @@ final class TranslatorServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(Translator::class, function (Application $app): Translator {
-            $config = $app->make(Config::class);
+        $this->app->bind(Translator::class, function (ContainerInterface $app): Translator {
+            $config = $app->make(ConfigInterface::class);
             $localeRaw = $config->get('app.locale', 'en');
             $fallbackRaw = $config->get('app.fallback_locale', 'en');
             $locale = is_string($localeRaw) ? $localeRaw : 'en';
             $fallback = is_string($fallbackRaw) ? $fallbackRaw : 'en';
-            $langPath = $app->basePath('lang');
+            $langPathRaw = $config->get('app.lang_path', '');
+            $langPath = is_string($langPathRaw) ? $langPathRaw : '';
 
             return new Translator($locale, $fallback, $langPath);
         });
