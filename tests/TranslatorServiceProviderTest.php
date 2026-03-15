8ade29b79b25bb7140cf703d0c7c52067e1334a0
home <andreas.uretschnig@gmail.com>
2026-03-15 01:47:35 +0100

    Remove unused imports and update `TranslatorServiceProviderTest` to extend `TestCase`.

--- tests/TranslatorServiceProviderTest.php
+++ tests/TranslatorServiceProviderTest.php
@@ -5,39 +5,14 @@ declare(strict_types=1);
 namespace Tests\I18n;
 
 use EzPhp\Application\Application;
-use EzPhp\Application\CoreServiceProviders;
-use EzPhp\Config\Config;
-use EzPhp\Config\ConfigLoader;
-use EzPhp\Config\ConfigServiceProvider;
-use EzPhp\Console\Command\MakeControllerCommand;
-use EzPhp\Console\Command\MakeMiddlewareCommand;
-use EzPhp\Console\Command\MakeMigrationCommand;
-use EzPhp\Console\Command\MakeProviderCommand;
-use EzPhp\Console\Command\MigrateCommand;
-use EzPhp\Console\Command\MigrateRollbackCommand;
-use EzPhp\Console\Console;
-use EzPhp\Console\ConsoleServiceProvider;
-use EzPhp\Console\Input;
-use EzPhp\Console\Output;
-use EzPhp\Container\Container;
-use EzPhp\Database\Database;
-use EzPhp\Database\DatabaseServiceProvider;
 use EzPhp\Exceptions\ApplicationException;
 use EzPhp\Exceptions\ContainerException;
-use EzPhp\Exceptions\DefaultExceptionHandler;
-use EzPhp\Exceptions\ExceptionHandlerServiceProvider;
 use EzPhp\I18n\Translator;
 use EzPhp\I18n\TranslatorServiceProvider;
-use EzPhp\Migration\MigrationServiceProvider;
-use EzPhp\Migration\Migrator;
-use EzPhp\Routing\Route;
-use EzPhp\Routing\Router;
-use EzPhp\Routing\RouterServiceProvider;
-use EzPhp\ServiceProvider\ServiceProvider;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\UsesClass;
 use ReflectionException;
-use Tests\DatabaseTestCase;
+use Tests\TestCase;
 
 /**
  * Class TranslatorServiceProviderTest
@@ -46,35 +21,7 @@ use Tests\DatabaseTestCase;
  */
 #[CoversClass(TranslatorServiceProvider::class)]
 #[UsesClass(Translator::class)]
-#[UsesClass(Application::class)]
-#[UsesClass(Container::class)]
-#[UsesClass(CoreServiceProviders::class)]
-#[UsesClass(Config::class)]
-#[UsesClass(ConfigLoader::class)]
-#[UsesClass(ConfigServiceProvider::class)]
-#[UsesClass(Database::class)]
-#[UsesClass(DatabaseServiceProvider::class)]
-#[UsesClass(MigrationServiceProvider::class)]
-#[UsesClass(Migrator::class)]
-#[UsesClass(RouterServiceProvider::class)]
-#[UsesClass(Route::class)]
-#[UsesClass(Router::class)]
-
-#[UsesClass(DefaultExceptionHandler::class)]
-#[UsesClass(ExceptionHandlerServiceProvider::class)]
-#[UsesClass(ConsoleServiceProvider::class)]
-#[UsesClass(Console::class)]
-#[UsesClass(MigrateCommand::class)]
-#[UsesClass(MigrateRollbackCommand::class)]
-#[UsesClass(MakeMigrationCommand::class)]
-
-#[UsesClass(MakeControllerCommand::class)]
-#[UsesClass(MakeMiddlewareCommand::class)]
-#[UsesClass(MakeProviderCommand::class)]
-#[UsesClass(Input::class)]
-#[UsesClass(Output::class)]
-#[UsesClass(ServiceProvider::class)]
-final class TranslatorServiceProviderTest extends DatabaseTestCase
+final class TranslatorServiceProviderTest extends TestCase
 {
     /**
      * @throws ReflectionException
